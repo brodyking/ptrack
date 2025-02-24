3719ee91abd2066ad6052a07b751a71965603074
brody king <46272894+brodyking@users.noreply.github.com>
2025-02-24 02:11:24 -0600

    Added Graph
    Shows pouches/day on homepage with Chart.js

--- app.php
+++ app.php
@@ -100,6 +100,78 @@
             font-size: 12pt;
           }
         </style>
+
+        <h5 class="card-title">Pouches</h5>
+        <canvas id="poucheschart" style="width:100%;max-width:700px;" class="border"></canvas>
+        <br>
+        <script src="assets/js/chart.umd.js"></script>
+        <?php
+
+        $graphxvals = "[";
+        $graphyvals = "[";
+        $history = pouchGetHistoryArray($username);
+
+        for ($i = 0; $i < sizeof($history); $i++) {
+          if ($i == sizeof($history) - 1) {
+            $graphxvals = $graphxvals . "'" . $history[$i] . "']";
+          } else {
+            $graphxvals = $graphxvals . "'" . $history[$i] . "', ";
+          }
+        }
+
+        for ($i = 0; $i < sizeof($history); $i++) {
+          $graphyvalscurrent = pouchGetPouches($username, $history[$i]);
+          if ($i == sizeof($history)-1) {
+            $graphyvals = $graphyvals . $graphyvalscurrent . "]";
+          } else {
+            $graphyvals = $graphyvals . $graphyvalscurrent . ", ";
+          }
+        }
+
+
+        ?>
+        <script>
+          Chart.defaults.color = 'white';
+          Chart.defaults.borderColor = '#495057';
+          
+          const xValues = <?php echo $graphxvals; ?>;
+          const yValues = <?php echo $graphyvals; ?>;
+          new Chart("poucheschart", {
+            type: "line",
+            data: {
+              labels: xValues,
+              datasets: [{
+                fill: false,
+                lineTension: 0,
+                backgroundColor: "white",
+                borderColor: "#0d6efd",
+                data: yValues
+              }]
+            },
+            options: {
+              responsive: true,
+              scales: {
+                y: {
+                  min: 0,
+                  suggestedMax: 6,
+                  ticks: {
+                    stepSize: 1
+                  }
+                }
+              },
+              plugins: {
+                legend: {
+                  display: false
+                }
+              },
+              layout: {
+                padding: 20
+              }
+            }
+          });
+
+        </script>
+        <br>
         <table class="table table-bordered table-striped">
           <tr>
             <th>Date</th>
@@ -113,15 +185,14 @@
 
           <?php
 
-          $history = pouchGetHistoryArray($username);
 
           for ($i = 0; $i < sizeof($history); $i++) {
             $historydate = $history[$i];
 
-            $historytotalmgs = pouchGetMgs($username,$historydate);
-            $historytotalpouches = pouchGetPouches($username,$historydate);
+            $historytotalmgs = pouchGetMgs($username, $historydate);
+            $historytotalpouches = pouchGetPouches($username, $historydate);
 
-            echo "<tr><td>".$historydate."</td><td>".$historytotalpouches."</td><td>".$historytotalmgs."</td></tr>";
+            echo "<tr><td>" . $historydate . "</td><td>" . $historytotalpouches . "</td><td>" . $historytotalmgs . "</td></tr>";
           }
 
           ?>
@@ -200,6 +271,9 @@
     </p>
 
     <script src="assets/js/bootstrap.js"></script>
+
+
+
 
 </body>
 
