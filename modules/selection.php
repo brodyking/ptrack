8f3ee3b2d932a3b2a3c419b55ac10a22e62caf95
brody king <46272894+brodyking@users.noreply.github.com>
2025-02-24 21:48:17 -0600

    Made buttons larger

--- modules/selection.php
+++ modules/selection.php
@@ -1,12 +1,14 @@
     <div class="card">
       <h5 class="card-header">Welcome back, <span class="text-primary"><?php echo $username ?></span>!</h5>
       <div class="card-body">
+      <div class="btn-group d-flex justify-content-center" role="group" aria-label="Basic mixed styles example">
         <a href="count.php?strength=6&<?php echo "username=" . $username . "&id=" . $id ?>"
-          class="btn btn-success">6mg</a>
+          class="btn btn-lg btn-success">6mg</a>
         <a href="count.php?strength=9&<?php echo "username=" . $username . "&id=" . $id ?>"
-          class="btn btn-primary">9mg</a>
+          class="btn btn-lg btn-primary">9mg</a>
         <a href="count.php?strength=12&<?php echo "username=" . $username . "&id=" . $id ?>"
-          class="btn btn-danger">12mg</a>
-        <a data-bs-toggle="modal" data-bs-target="#reset" class="btn btn-secondary">Reset Today</a>
+          class="btn btn-lg btn-danger">12mg</a>
+        <a data-bs-toggle="modal" data-bs-target="#reset" class="btn btn-lg btn-secondary">Reset</a>
+        </div>
       </div>
     </div>
