4f170a80450b0574662f91f99d4360b9a00cb5b0
brody king <46272894+brodyking@users.noreply.github.com>
2025-02-24 21:47:37 -0600

    Moved logout to last item in footer list

--- modules/footer.php
+++ modules/footer.php
@@ -1,10 +1,10 @@
 <p style="margin-top:30px;" class="text-center">
-      <a style="text-decoration: none;" href="logout.php?username=<?php echo $username ?>">Logout</a>
-      &middot;
-      <a style="text-decoration: none;" href="paperwork.php">Terms of Service</a>
-      &middot;
-      <a style="text-decoration: none;" href="changepswd.php?username=<?php echo $username . "&id=" . $id ?>">Change
+    <a style="text-decoration: none;" href="paperwork.php">Terms of Service</a>
+    &middot;
+    <a style="text-decoration: none;" href="changepswd.php?username=<?php echo $username . "&id=" . $id ?>">Change
         Password</a>
-      &middot;
-      <a style="text-decoration: none;" href="#" data-bs-toggle="modal" data-bs-target="#delete">Delete Account</a>
-    </p>
+    &middot;
+    <a style="text-decoration: none;" href="#" data-bs-toggle="modal" data-bs-target="#delete">Delete Account</a>
+    &middot;
+    <a style="text-decoration: none;" href="logout.php?username=<?php echo $username ?>">Logout</a>
+</p>
