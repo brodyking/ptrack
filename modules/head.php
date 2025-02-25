101c67d15a5c6da5e5ac58f47280061ebbb46a86
brody king <46272894+brodyking@users.noreply.github.com>
2025-02-24 21:47:59 -0600

    Changed version from <sub> to a badge

--- modules/head.php
+++ modules/head.php
@@ -1,3 +1,3 @@
-<h1 class="mb-3 fw-semibold lh-1 text-center" style="margin-top:50px;font-size:28pt;">Pouchtrack <sub class="text-primary">v3</sub></h1>
+<h1 class="mb-3 fw-semibold lh-1 text-center" style="margin-top:50px;font-size:28pt;">Pouchtrack <span class="fs-6 badge text-bg-warning 10px">v3</span></h1>
 <p class="text-center" style="font-family: 'Times New Roman', Times, serif;font-style: italic;font-size:13pt;">Track
     your nicotine intake with ease.</p>
