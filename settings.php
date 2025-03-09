b207702e18290921e97d14d9f46a830cbdabbcdf
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-09 03:07:28 -0500

    Get your pouch usage data on settings

--- settings.php
+++ settings.php
@@ -21,22 +21,22 @@
 
     // Verify that userrname and ID are present in URL
     if (!isset($_GET['username']) || !isset($_GET['id'])) {
-        header("Location: index.php?error=invalidsession");
+      header("Location: index.php?error=invalidsession");
     }
     // Set variables
     $username = $_GET['username'];
     $id = $_GET['id'];
     // Check if users session is valid
     if (userSessionGet($username) != $id) {
-        // Reset the session for security purposes.
-        userSessionClear($username);
-        // Redirect to login
-        header("Location: index.php?error=invalidsession");
+      // Reset the session for security purposes.
+      userSessionClear($username);
+      // Redirect to login
+      header("Location: index.php?error=invalidsession");
     }
-    $id = userSessionSecureInit($username); 
+    $id = userSessionSecureInit($username);
     //Creates new day if it does not exist
     if (!pouchExists($username, date("m-d-Y"))) {
-        pouchInit($username, date("m-d-Y"));
+      pouchInit($username, date("m-d-Y"));
     }
     ?>
     <!-- Pouch Selection -->
@@ -62,57 +62,75 @@
         <input type="text" class="form-control" disabled value="<?php echo $id; ?>">
         <br>
         <h5 class="card-title">Secure Session</h5>
-        <p>Secure Session changes your Session ID on every pageload. This is more secure, but refreshing and bookmarks do not work. Use at your own risk.</p>
+        <p>Secure Session changes your Session ID on every pageload. This is more secure, but refreshing and bookmarks
+          do not work. Use at your own risk.</p>
         <form method="POST" action="secureid.php" class="row p-0">
           <div class="col-auto pe-0">
             <select name="issecure" class="form-select form-select bg-light-subtle" aria-label="Small select example">
               <?php if (userSessionSecureGet($username)) {
-                  echo '<option selected value="true">Enabled</option><option value="false">Disabled</option>';
+                echo '<option selected value="true">Enabled</option><option value="false">Disabled</option>';
               } else {
-                  echo '<option value="true">Enabled</option><option selected value="false">Disabled</option>';
+                echo '<option value="true">Enabled</option><option selected value="false">Disabled</option>';
               } ?>
             </select>
             <input type="text" style="display: none" name="username" value="<?php echo $username; ?>">
             <input type="text" style="display: none" name="password" value="<?php echo userPasswordGet($username); ?>">
           </div>
-            <div class="col-auto">
-              <button type="submit" class="btn btn-primary mb-3">Change</button>
-            </div>
-          </form>
+          <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Change</button>
+          </div>
+        </form>
         <h5 class="card-title">Change Password</h5>
         <p>You may change your password here. Once changed, you will be logged out everywhere.</p>
         <a class="btn btn-primary" style="text-decoration: none;"
           href="changepswd.php?username=<?php echo $username . "&id=" . $id; ?>">Change
           Password</a>
         <br><br>
-        <h5 class="card-title">Delete Account</h5>
-        <a class="btn btn-danger" style="text-decoration: none;" href="#" data-bs-toggle="modal"
-          data-bs-target="#delete">Delete Account</a>
-      </div>
-    </div>
-  </div>
-
-  <!-- Delete Account popup -->
-  <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="reset" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="delete">Warning</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <p>You are about to delete your account. This is permanent and you will not have access to your
-            information
-            once you click Delete. Do you wish to continue?</p>
-        </div>
-        <div class="modal-footer">
-          <a href="delete.php?<?php echo "username=" . $username . "&id=" . $id; ?>" class="btn btn-danger">Delete my Account</a>
+        <h5 class="card-title">Data</h5>
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item" style="border: none;">
+            <h2 class="accordion-header">
+              <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Pouch Usage (JSON) 
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body border mt-4 rounded">
+                <code><pre style="margin: 0px;"><?php echo file_get_contents("data/db_users/".$username."/pouches.json"); ?></pre></code>
+              </div>
+            </div>
+          </div>
+          <br>
+          <h5 class="card-title">Delete Account</h5>
+          <a class="btn btn-danger" style="text-decoration: none;" href="#" data-bs-toggle="modal"
+            data-bs-target="#delete">Delete Account</a>
         </div>
       </div>
     </div>
-  </div>
+    
+    <!-- Delete Account popup -->
+    <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="reset" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="delete">Warning</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>You are about to delete your account. This is permanent and you will not have access to your
+              information
+              once you click Delete. Do you wish to continue?</p>
+          </div>
+          <div class="modal-footer">
+            <a href="delete.php?<?php echo "username=" . $username . "&id=" . $id; ?>" class="btn btn-danger">Delete my
+              Account</a>
+          </div>
+        </div>
+      </div>
+    </div>
 
-  <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
 </body>
 
 </html>
