7c0561aad25ec697f30b7d31ad7e895db442c6bf
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-09 01:52:04 -0600

    Removed old "todays statistics"

--- app.php
+++ app.php
@@ -20,22 +20,22 @@
 
     // Verify that userrname and ID are present in URL
     if (!isset($_GET['username']) || !isset($_GET['id'])) {
-        header("Location: index.php?error=invalidsession");
+      header("Location: index.php?error=invalidsession");
     }
     // Set variables
     $username = $_GET['username'];
     $id = $_GET['id'];
     // Check if users session is valid
     if (userSessionGet($username) != $id) {
-        // Reset the session for security purposes.
-        userSessionClear($username);
-        // Redirect to login
-        header("Location: index.php?error=invalidsession");
+      // Reset the session for security purposes.
+      userSessionClear($username);
+      // Redirect to login
+      header("Location: index.php?error=invalidsession");
     }
     $id = userSessionSecureInit($username);
     //Creates new day if it does not exist
     if (!pouchExists($username, date("m-d-Y"))) {
-        pouchInit($username, date("m-d-Y"));
+      pouchInit($username, date("m-d-Y"));
     }
     ?>
     <!-- Navbar -->
@@ -51,9 +51,6 @@
         <div class="col-md ps-0">
           <!-- Pouch Selection -->
           <?php include "modules/selection.php"; ?>
-          <br>
-          <!-- Statistics for currrent day -->
-          <?php include "modules/statistics.php"; ?>
         </div>
         &nbsp;
         <div class="col-md pe-0">
@@ -68,7 +65,8 @@
       <!-- Footer -->
       <?php include "modules/footer.php"; ?>
       <script src="assets/js/bootstrap.js"></script>
-      </div></div>
+    </div>
+  </div>
 </body>
 
 </html>
