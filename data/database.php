31048c28073957b6b8d57479fb34871d535f5ea5
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-09 01:45:14 -0600

    New Database using JSON. WAYYY BETTER.

--- data/database.php
+++ data/database.php
@@ -30,65 +30,84 @@ function getSiteVersion() {
 // USER FUNCTIONS
 function userPathTo($username)
 {
-    return 'data/db_users/@' . $username . '/';
+    return 'data/db_users/' . $username . '/';
 }
 function userExists($username)
 {
     $pathto = userPathTo($username);
-    return file_exists($pathto . $username . '.password');
+    return file_exists($pathto . 'account.json');
+}
+function userSettingsGet($username,$key) {
+    $pathto = userPathTo($username);
+    $account = json_decode(read($pathto."account.json"),true);
+    return $account[$key];
+}
+function userSettingsSet($username,$key,$value) {
+    $pathto = userPathTo($username);
+    $account = json_decode(read($pathto."account.json"),true);
+    $account[$key] = $value; 
+    $account = json_encode($account,JSON_PRETTY_PRINT);
+    write($pathto."account.json",$account);
 }
 function userAuth($username, $password)
 {
-    $pathto = userPathTo($username);
-    $realpassword = read($pathto . $username . '.password');
-    return $realpassword == $password;
+    return $password == userSettingsGet($username,"password");
 }
 function userCreate($username, $password)
 {
     $pathto = userPathTo($username);
     mkdir($pathto);
-    write($pathto . $username . '.password', $password);
-    write($pathto . $username . '.joindate', date("m-d-Y"));
-    write($pathto . $username . '.secureid',"true");
+    write($pathto."pouches.json","");
+    $accountdata = array();
+    $accountdata["username"] = $username;
+    $accountdata["password"] = $password;
+    $accountdata["joindate"] = date("m-d-Y");
+    $accountdata["secureid"] = "true";
+    $accountdata["isdeleted"] = "false";
+    $accountdata = json_encode($accountdata,JSON_PRETTY_PRINT);
+    write($pathto."account.json",$accountdata);
     return 0;
 }
 function userDelete($username)
 {
-
-    $pathto = userPathTo($username);
-    write($pathto . $username . '.password', random_int(10000000, 10000000000000));
-    write($pathto . $username . '.isdeleted', "true");
-
+    userSettingsSet($username,"password",(string)random_int(100000000,100000000000));
+    userSettingsSet($username,"isdeleted","true");
 }
 function userSessionInit($username, $id)
 {
     $pathto = userPathTo($username);
-    write($pathto . $username . '.session', $id);
+    $idtosend = array();
+    $idtosend["id"] = $id;
+    write($pathto . 'session.json',json_encode($idtosend,JSON_PRETTY_PRINT));
     return 0;
 }
 
 function userSessionClear($username)
 {
     $pathto = userPathTo($username);
-    unlink($pathto . $username . '.session');
+    unlink($pathto . 'session.json');
     return 0;
 }
 
 function userSessionGet($username)
 {
     $pathto = userPathTo($username);
-    if (file_exists($pathto . $username . ".session")) {
-        $session = read($pathto . $username . ".session");
-        return $session;
+    if (file_exists($pathto . "session.json")) {
+        $session = read($pathto . "session.json");
+        $session = json_decode($session,true);
+        return $session["id"];
     }
     return -1;
 }
 function userSessionCreate($username)
 {
     $pathto = userPathTo($username);
-    $newid = random_int(100000000, 1000000000);
-    write($pathto . $username . ".session", $newid);
-    return $newid;
+    $newid = array();
+    $newidint = random_int(100000000, 1000000000);
+    $newid["id"] =  $newidint;
+    $newid = json_encode($newid,JSON_PRETTY_PRINT);
+    write($pathto . "session.json", $newid);
+    return $newidint;
 }
 function userSessionSecureInit($username) {
     if (userSessionSecureGet($username)) {
@@ -98,116 +117,71 @@ function userSessionSecureInit($username) {
     }
 }
 function userSessionSecureGet($username) {
-
-    $pathto = userPathTo($username);
-    $value = read($pathto . $username . '.secureid');
-    if ($value == "true") {
-        return true;
-    }
-    return false;
+    $issecuresession = userSettingsGet($username,"secureid");
+    return ($issecuresession == "true");
 }
 function userSessionSecureSet($username,$value) {
-    $pathto = userPathTo($username);
-    if ($value == "true") {
-        write($pathto.$username.".secureid","true");
-    } else {
-        write($pathto.$username.".secureid","false");
-    }
+   userSettingsSet($username,"secureid",$value);
 }
 function userPasswordGet($username) {
-    $pathto = userPathTo($username);
-    return read($pathto.$username.".password");
+    userSettingsGet($username,"password");
 }
 function userChangePassword($username, $password)
 {
-    $pathto = userPathTo($username);
-    file_put_contents($pathto . $username . ".password", $password);
-    return 0;
+    userSettingsSet($username,"password",$password);
 }
 
 function userIsAdmin($username)
 {
-    $pathto = userPathTo($username);
-    return (file_exists($pathto . $username . ".isadmin"));
+    $isadmin = userSettingsGet($username,"isadmin");
+    return ($isadmin == "true");
 }
 
 function userJoinDate($username)
 {
-    $pathto = userPathTo($username);
-    return read($pathto . $username . ".joindate");
+    userSettingsGet($username,"joindate");
 }
 
 // POUCH FUNCTIONS
 function pouchInit($username, $day)
 {
     $pathto = userPathTo($username);
-    mkdir($pathto . '/' . $day);
-    $pathto = $pathto . '/' . $day . '/';
-    write($pathto . $username . ".totalpouches", 0);
-    write($pathto . $username . ".totalmgs", 0);
+    $old = json_decode(read($pathto."pouches.json"),true);
+    $old[$day] = [0,0];
+    $new = json_encode($old,JSON_PRETTY_PRINT);
+    write($pathto."pouches.json",$new);   
     return 0;
 }
 function pouchExists($username, $day)
 {
     $pathto = userPathTo($username);
-    $pathto = $pathto . '/' . $day . '/';
-    return (file_exists($pathto . $username . ".totalpouches") && file_exists($pathto . $username . ".totalmgs"));
+    $old = json_decode(read($pathto."pouches.json"),true);
+    return isset($old[$day]);
+
 }
-function pouchAdd($username, $day, $strength)
-{
+function pouchAdd($username,$day,$strength) {
     $pathto = userPathTo($username);
-    $pathto = $pathto . '/' . $day . '/';
-
-    $currentpouches = read($pathto . $username . ".totalpouches");
-    $currentmgs = read($pathto . $username . ".totalmgs");
-
-    $currentpouches += 1;
-    $currentmgs += $strength;
-
-    write($pathto . $username . ".totalpouches", $currentpouches);
-    write($pathto . $username . ".totalmgs", $currentmgs);
-
-    return 0;
+    $old = json_decode(read($pathto."pouches.json"),true);
+    $old[$day] = array((int)$old[$day][0] + 1,(int)$old[$day][1] + $strength);
+    $new = json_encode($old,JSON_PRETTY_PRINT);
+    write($pathto."pouches.json",$new);
 }
 function pouchGetMgs($username, $day)
 {
     $pathto = userPathTo($username);
-    $pathto = $pathto . '/' . $day . '/';
-    return read($pathto . $username . ".totalmgs");
+    $old = json_decode(read($pathto."pouches.json"),true);
+    return $old[$day][1];
 }
 function pouchGetPouches($username, $day)
 {
     $pathto = userPathTo($username);
-    $pathto = $pathto . '/' . $day . '/';
-    return read($pathto . $username . ".totalpouches");
-}
-function pouchGetHistoryString($username)
-{
-    $pathto = userPathTo($username);
-    $history = scandir($pathto);
-    $historyString = "";
-    for ($i = 2; $i < sizeof($history); $i++) {
-        if (!is_dir($pathto . $history[$i])) {
-            break;
-        }
-        $historyString = $historyString . $history[$i] . ", ";
-    }
-    return $historyString;
+    $old = json_decode(read($pathto."pouches.json"),true);
+    return $old[$day][0];
 }
 function pouchGetHistoryArray($username)
 {
     $pathto = userPathTo($username);
-    $history = scandir($pathto);
-    $historyArray = array();
-    $spotinnewarray = 0;
-    for ($i = 2; $i < sizeof($history); $i++) {
-        if (!is_dir($pathto . $history[$i])) {
-            break;
-        }
-        $historyArray[$spotinnewarray] = $history[$i];
-        $spotinnewarray++;
-    }
-    return $historyArray;
+    $old = json_decode(read($pathto."pouches.json"),true);
+    return array_keys($old);
 }
-
 // ?>
