19d57df6c798587f56a6df9c3e51179926e47ce0
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-19 19:52:21 -0500

    Splash Title Changes

--- modules/source/splash.php
+++ modules/source/splash.php
@@ -1,10 +1,14 @@
-<h1 class="mb-3 fw-semibold lh-1 text-center text-gradient"
-    style="  background: -webkit-linear-gradient(#77a6f7,#106EFD);-webkit-background-clip: text;-webkit-text-fill-color: transparent;margin-top:50px;font-size:28pt;">
-    Next-Generation Nicotine Tracking</h1>
-<p class="text-center" style="font-size:13pt;margin-bottom: 0px;">Track your nicotine habits, with ease.</p>
+<h1 class="mb-3 lh-1 text-center text-gradient splash-title timesnewroman">
+   Next-Generation Nicotine Tracking 
+</h1>
+<p class="text-center junicode" style="font-size:13pt;margin-bottom: 0px;font-style:italic;">
+    Track your nicotine habits, with ease.
+</p>
 <div class="container p-3 text-center">
-    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#register"><i
-            class="bi bi-person-add"></i> Create an Account</a>
-    <a href="#login" class="btn btn-secondary loginbuttonsplash"><i
-            class="bi bi-person-add"></i> Login</a>
+    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#register">
+        <i class="bi bi-person-add"></i> Create an Account
+    </a>
+    <a href="#login" class="btn btn-secondary loginbuttonsplash">
+        <i class="bi bi-person-add"></i> Login
+    </a>
 </div>
