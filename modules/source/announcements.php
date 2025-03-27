1661794690f75998865097ef45410da1f31ae45c
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-27 15:58:20 -0500

    Old announcements I forgot to add

--- modules/source/announcements.php
+++ modules/source/announcements.php
@@ -1,19 +1,27 @@
 <div class="row g-2" style="margin: 0px!important;">
-        <div class="col-md ps-0">
-              <div class="card">
-      <h5 class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-megaphone-fill" viewBox="0 0 16 16" style="vertical-align: 0%;">
-        <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0zm-1 .724c-2.067.95-4.539 1.481-7 1.656v6.237a25 25 0 0 1 1.088.085c2.053.204 4.038.668 5.912 1.56zm-8 7.841V4.934c-.68.027-1.399.043-2.008.053A2.02 2.02 0 0 0 0 7v2c0 1.106.896 1.996 1.994 2.009l.496.008a64 64 0 0 1 1.51.048m1.39 1.081q.428.032.85.078l.253 1.69a1 1 0 0 1-.983 1.187h-.548a1 1 0 0 1-.916-.599l-1.314-2.48a66 66 0 0 1 1.692.064q.491.026.966.06"></path>
-      </svg> Announcements</h5>
+  <div class="col-md ps-0">
+    <div class="card">
+      <h5 class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+          class="bi bi-megaphone-fill" viewBox="0 0 16 16" style="vertical-align: 0%;">
+          <path
+            d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0zm-1 .724c-2.067.95-4.539 1.481-7 1.656v6.237a25 25 0 0 1 1.088.085c2.053.204 4.038.668 5.912 1.56zm-8 7.841V4.934c-.68.027-1.399.043-2.008.053A2.02 2.02 0 0 0 0 7v2c0 1.106.896 1.996 1.994 2.009l.496.008a64 64 0 0 1 1.51.048m1.39 1.081q.428.032.85.078l.253 1.69a1 1 0 0 1-.983 1.187h-.548a1 1 0 0 1-.916-.599l-1.314-2.48a66 66 0 0 1 1.692.064q.491.026.966.06">
+          </path>
+        </svg> Announcements</h5>
       <div class="card-body">
-        <h5 class="card-title">Recent Updates v4.1</h5>
-        <p>This update includes general bug fixes and slight improvements.</p>
-        <h5 class="card-title">Recent Updates v4.0</h5>
-        <p>v4.0 is a massive rewrite for almost every point on the application.
+      <h5 class="card-title">Recent Updates v4.2</h5>
+        <p>This feature update includes many new features.
           <ul>
-            <li>New Database and UI</li>
-            <li>Logo Change</li>
-          </ul>
+
+            <li>Ability to track cans, alongside pouches</li>
+            <li>This also includes new layout controls for the homepage</li>
+            
+          <li>Small UI tweaks</li>
+          <li>Settings page now renders fullscreen on mobile</li>
+          <li>Brought back old gradient from 4.0</li>
+          <li>More Icons and Username field</li>
+          <li>Navigation bar (while logged in) is more concise.</li>
+        </ul>
         </p>
-     </div>
+      </div>
     </div>
-</div>
+  </div>
