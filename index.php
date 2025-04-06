996e3d9e51ae711c3294c48cff291f29fe15899b
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-05 21:44:48 -0500

    Meow

--- index.php
+++ index.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php
 
-        /*
+<head>
+    <?php
 
-        (c) 2025 Brody King. All Rights Reserved
+    /*
 
-        https://github.com/brodyking/ptrack/
+    (c) 2025 Brody King. All Rights Reserved
 
-        */
+    https://github.com/brodyking/ptrack/
 
-        // Database
-        include "data/database.php";
-        // Modules
-        include "modules/modules.php";
-        // Scripts
-        include "scripts/scripts.php";
+    */
 
-        // Script Importing 
-        include scriptsGet("isloggedin");
-        include scriptsGet("error");
-        include scriptsGet("checktoday");
+    // Database
+    include "data/database.php";
+    // Modules
+    include "modules/modules.php";
+    // Scripts
+    include "scripts/scripts.php";
 
-        ?>
-        <link href="assets/css/bootstrap.css" rel="stylesheet">
-        <link href="assets/icons/font/bootstrap-icons.min.css" rel="stylesheet">
-        <link href="assets/main.css" rel="stylesheet">
-        <title>Pouchtrack</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/x-icon" href="assets/logo.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/logo-180.png">
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    </head>
+    // Script Importing 
+    include scriptsGet("isloggedin");
+    include scriptsGet("error");
+    include scriptsGet("checktoday");
+
+    ?>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/icons/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="assets/main.css" rel="stylesheet">
+    <title>Pouchtrack</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="assets/logo.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/logo-180.png">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+</head>
 
 <body data-bs-theme="dark">
     <main>
@@ -48,14 +50,10 @@
         // Check if logged in
         if (isLoggedIn()) {
 
-            $username = $_GET['username'];
+            $username = $_COOKIE['username'];
 
             // Secure Session ID Changing
-            if (userSessionSecureGet($username)) {
-                $id = userSessionCreate($username);
-            } else {
-                $id = $_GET['id'];
-            }
+            $id = $_COOKIE['id'];
 
 
             // Checks if entry for current date is set, if not, creates a blank one
@@ -64,6 +62,7 @@
             // Rendered Items when logged in
             include modulesGetPath("nav-loggedin");
             include modulesGetPath("settings");
+            include modulesGetPath("install");
             include modulesGetPath("welcome");
             include modulesGetPath("dashboard");
 
@@ -81,7 +80,7 @@
         ?>
     </main>
     <?php
-        include modulesGetPath("footer");
+    include modulesGetPath("footer");
     ?>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
 
