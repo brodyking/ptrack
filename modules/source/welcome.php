4feb6575d8e482b74bc4ee512841f028267a8c97
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-12 17:18:46 -0500

    Font size increase

--- modules/source/welcome.php
+++ modules/source/welcome.php
@@ -1,10 +1,9 @@
 <div class="container p-0 m-0 mb-4 text-center" style="min-width: 100%;">
-  <h1 class="mb-3 fw-semibold lh-1 text-gradient"
-    style="  background: -webkit-linear-gradient(#ffffff,rgb(138, 142, 146));-webkit-background-clip: text;-webkit-text-fill-color: transparent;margin-top:50px;font-size:28pt;">
-    Welcome back, <span
-      style="background: -webkit-linear-gradient(#98C2FD,#0D70FD);-webkit-background-clip: text;-webkit-text-fill-color: transparent;"><?php echo $username; ?></span>.
+  <h1 class="mb-3 fw-semibold lh-1 text-gradient splash-title-secondary" style="margin-top:50px;font-size:38pt;">
+    Welcome back, <span class="splash-title-primary"><?php echo $username; ?></span>.
   </h1>
   <p class="text-center p-0 m-0" style="font-size:13pt;">You have used <span
       class="text-primary-emphasis"><?php echo pouchGetPouches($username, date("m-d-Y")); ?></span> total pouches, and
-    <span class="text-primary-emphasis"><?php echo pouchGetMgs($username, date("m-d-Y")); ?></span> total mgs today.</p>
+    <span class="text-primary-emphasis"><?php echo pouchGetMgs($username, date("m-d-Y")); ?></span> total mgs today.
+  </p>
 </div>
