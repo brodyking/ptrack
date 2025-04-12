ac86e6d46570d0378bf7d0b16b69c7f8fbc12df5
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-12 17:17:25 -0500

    Seperate Pages

--- modules/source/register.php
+++ modules/source/register.php
@@ -1,27 +1,30 @@
-<div class="modal fade" id="register" tabindex="-1" aria-labelledby="register" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="register"><i class="bi bi-door-open-fill"></i> Register</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div class="card mt-5" id="login" style="max-width: 400px; margin: auto;">
+  <h5 class="card-header"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      class="bi bi-key-fill" viewBox="0 0 16 16" style="vertical-align: 0%;">
+      <path
+        d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2M2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2">
+      </path>
+    </svg> Register</h5>
+  <div class="card-body">
+    <form method="POST" action="api.php?action=register">
+      <div class="mb-3">
+        <label for="recipient-name" class="col-form-label">Username</label>
+        <input type="text" class="form-control" name="username">
       </div>
-      <div class="modal-body">
-        <form method="POST" action="api.php?action=register">
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">Username</label>
-            <input type="text" class="form-control" name="username">
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-          </div>
-          <button type="submit" class="btn btn-primary">Register</button>
-        </form>
+      <div class="mb-3">
+        <label for="message-text" class="col-form-label">Password</label>
+        <input type="password" class="form-control" name="password">
       </div>
-      <div class="modal-footer">
-        <i>By clicking Register, you agree to the Terms of Service, Privacy Policy, and Cookie Policy located at the
-          bottom of this page.</i>
+      <div class="mb-3">
+        By clicking Register, you agree to the Terms of Service, Privacy Policy, and Cookie Policy located <a
+          href="/?paperwork">here</a>.
       </div>
-    </div>
+      <div class="mb-3">
+        <button type="submit" class="btn btn-secondary-new">Register</button>
+      </div>
+      <div class="mb-3">
+        Already have an account? <a href="/?login">Click here</a>.
+      </div>
+    </form>
   </div>
 </div>
