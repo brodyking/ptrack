5dc03424790e56d8af57b929245ade11d9bd5b3d
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-12 17:17:54 -0500

    Now able to view on mobile

--- modules/source/nav-splash.php
+++ modules/source/nav-splash.php
@@ -1,25 +1,36 @@
-<div class="card">
-  <nav class="navbar navbar-expand-lg rounded card-header p-0 border-bottom-0">
-    <div class="container-fluid pe-1 ps-2">
-      <a class="navbar-brand me-2" href="index.php">
-        <img src="/assets/logo.png" alt="Logo" height="24" class="d-inline-block align-text-top me-1">
-        Pouchtrack
-      </a>
-      <!--<button class="navbar-toggler navbar-toggler-sm" type="button" data-bs-toggle="collapse"data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"aria-label="Toggle navigation"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" style="vertical-align: -10%;"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/></svg> -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">
-              <i class="bi bi-house-fill"></i> Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#cookies">
-              <i class="bi bi-paperclip"></i> Paperwork</a>
-          </li>
-        </ul>
-      </div>
-      <a href="https://github.com/brodyking/ptrack/" class="btn btn-sm btn-primary ">Support on Github <i
-          class="bi bi-github"></i></a>
+<nav id="nav" class="navbar navbar-expand-lg bg-body-tertiary border rounded"
+  style="background-color: #dee2e608!important;">
+  <div class="container-fluid pe-1 ps-2">
+    <a class="navbar-brand" href="/">
+      <img src="/assets/logo.png" alt="Logo" width="24" height="24" class="d-inline-block align-text-top">
+      Pouchtrack</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      Menu <i class="bi bi-list"></i></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">
+            Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/?paperwork">
+            Paperwork</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/?changes">
+            Changelog</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/?login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/?register">Register</a>
+        </li>
+      </ul>
     </div>
-  </nav>
-</div>
+  </div>
+</nav>
