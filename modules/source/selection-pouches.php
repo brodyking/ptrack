66589592d23d5d9cd03cef2108e942177d933b38
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-12 17:18:00 -0500

    Buttons

--- modules/source/selection-pouches.php
+++ modules/source/selection-pouches.php
@@ -6,17 +6,17 @@
   <div class="card-body">
 
     <div class="selection d-grid d-md-flex justify-content-md-center">
-      <a href="api.php?action=count&strength=6" class="btn btn-lg btn-primary w-100 text-start text-center"><i
+      <a href="api.php?action=count&strength=6" class="btn btn-lg btn-secondary-new w-100 text-start text-center"><i
           class="bi bi-plus-circle-fill"></i> 6mg</a>
       <br>
-      <a href="api.php?action=count&strength=9" class="btn btn-lg btn-primary w-100 text-start text-center"><i
+      <a href="api.php?action=count&strength=9" class="btn btn-lg btn-secondary-new w-100 text-start text-center"><i
           class="bi bi-plus-circle-fill"></i> 9mg</a>
       <br>
-      <a href="api.php?action=count&strength=12" class="btn btn-lg btn-primary w-100 text-start text-center"><i
+      <a href="api.php?action=count&strength=12" class="btn btn-lg btn-secondary-new w-100 text-start text-center"><i
           class="bi bi-plus-circle-fill"></i> 12mg</a>
       <br>
-      <a data-bs-toggle="modal" data-bs-target="#reset" class="btn btn-lg btn-danger w-100 text-start text-center"><i
-          class="bi bi-x-circle-fill"></i> Reset</a>
+      <a data-bs-toggle="modal" data-bs-target="#reset"
+        class="btn btn-lg btn-primary-new w-100 text-start text-center"><i class="bi bi-x-circle-fill"></i> Reset</a>
     </div>
   </div>
 </div>
