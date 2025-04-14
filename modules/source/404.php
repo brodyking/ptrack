b6e7c3bbc2a14abf6560b360ce384191bfa257f1
brody king <46272894+brodyking@users.noreply.github.com>
2025-04-13 19:58:29 -0500

    new 404 page

---
+++ modules/source/404.php
@@ -0,0 +1,12 @@
+<style>
+    body {
+        background-image: radial-gradient(circle at 90% 24%, rgba(209, 209, 209, 0.04) 0%, rgba(209, 209, 209, 0.04) 50%, rgba(160, 160, 160, 0.04) 50%, rgba(160, 160, 160, 0.04) 100%), radial-gradient(circle at 91% 63%, rgba(45, 45, 45, 0.04) 0%, rgba(45, 45, 45, 0.04) 50%, rgba(87, 87, 87, 0.04) 50%, rgba(87, 87, 87, 0.04) 100%), radial-gradient(circle at 17% 2%, rgba(124, 124, 124, 0.04) 0%, rgba(124, 124, 124, 0.04) 50%, rgba(117, 117, 117, 0.04) 50%, rgba(117, 117, 117, 0.04) 100%), linear-gradient(88deg, rgb(33, 20, 105), rgb(1, 15, 13)) !important;
+    }
+</style>
+<div class="container p-3 text-center mt-5">
+    <h1 class="fw-semibold" style="font-size: 45pt;">Error 404</h1>
+    <p>Page not found.</p>
+    <a href="/" class="btn btn-secondary-new rounded-5 me-1" data-bs-toggle="modal" data-bs-target="/?register">
+        Back to Home <i class="bi bi-arrow-right-short"></i>
+    </a>
+</div>
