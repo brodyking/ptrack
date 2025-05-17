5c26e8c4e571854c43bdae58873f46b73660570f
brody king <46272894+brodyking@users.noreply.github.com>
2025-05-16 20:49:29 -0500

    Revert "Moved reset warnings to selection module"
    This reverts commit 1a341c41483072744aac22dc5878f05400ef6b22.

--- modules/source/graph-pouches.php
+++ modules/source/graph-pouches.php
@@ -177,3 +177,23 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="reset" tabindex="-1" aria-labelledby="reset" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="register"><i class="bi bi-exclamation-triangle-fill"></i> Warning
+                    </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>You are about to reset your <b>pouches</b> for today. You cannot undo this action. And no
+                        cheating!!</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="api.php?action=count&strength=reset" class="btn btn-danger">Reset</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
