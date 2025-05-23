048920d38c2de65431478a5028c05dc02e5db54f
brody king <46272894+brodyking@users.noreply.github.com>
2025-05-23 10:49:08 -0500

    Error codes are now shown directly on the API. No redirects. Passwords are starred out.

--- scripts/source/rawdata.php
+++ scripts/source/rawdata.php
@@ -3,13 +3,17 @@ function rawdata()
 {
 
     if (!isset($_GET["username"]) || !isset($_GET["id"]) || !isset($_GET["source"])) {
-        apiError("rawdata.missingparams");
+        $error = array("errorCode" => "rawdata.missingparams", "errorDetail" => "Missing parameters while accessing API.", "requiredParams" => ["username", "id", "source"]);
+        echo json_encode($error, JSON_PRETTY_PRINT);
     } else if (!userExists($_GET["username"])) {
-        apiError("rawdata.nouser");
+        $error = array("errorCode" => "rawdata.nouser", "errorDetail" => "No user exists under that username.");
+        echo json_encode($error, JSON_PRETTY_PRINT);
     } else if ($_GET["id"] != userSessionGet($_GET["username"])) {
-        apiError("rawdata.invalidsession");
+        $error = array("errorCode" => "rawdata.invalidsession", "errorDetail" => "Invalid session. You can find your Session ID in the Settings page.");
+        echo json_encode($error, JSON_PRETTY_PRINT);
     } else if (userSettingsGet($_GET["username"], "allowapi") == "false") {
-        apiError("rawdata.apidisabled");
+        $error = array("errorCode" => "rawdata.apidisabled", "errorDetail" => "Your account has the API disabled. You can enable it in the settings page.");
+        echo json_encode($error, JSON_PRETTY_PRINT);
     } else {
         switch ($_GET["source"]) {
             case "pouches":
@@ -21,12 +25,17 @@ function rawdata()
             case "account":
                 $accountdata = file_get_contents("data/db_users/{$_GET["username"]}/account.json");
                 $accountdata = json_decode($accountdata, true);
-                unset($accountdata["password"]);
+                $hiddenpassword = "";
+                for ($i = 0; $i < strlen($accountdata["password"]); $i++) {
+                    $hiddenpassword = $hiddenpassword . "*";
+                }
+                $accountdata["password"] = $hiddenpassword;
                 $accountdata = json_encode($accountdata, JSON_PRETTY_PRINT);
                 echo $accountdata;
                 break;
             default:
-                apiError("rawdata.invalidparams");
+                $error = array("errorCode" => "rawdata.invalidparams", "errorDetail" => "Invalid Source.", "sourceOptions" => ["pouches", "cans", "account"]);
+                echo json_encode($error, JSON_PRETTY_PRINT);
                 break;
         }
     }
