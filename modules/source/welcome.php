f6327f863b806821dc5efdcce0802568ae0ae35a
brody king <46272894+brodyking@users.noreply.github.com>
2025-05-26 00:13:03 -0500

    A little grammar edit. If only 1 pouch, no longer plural.

--- modules/source/welcome.php
+++ modules/source/welcome.php
@@ -1,10 +1,19 @@
 <div class="container p-0 m-0 mb-4 text-center" style="min-width: 100%;">
   <h1 class="mb-3 fw-semibold lh-1" style="margin-top:50px;font-size:38pt;">
-    Welcome back, <span class="splash-title-primary"><?php echo $username; ?></span>.
+    Welcome back, <span class="splash-title-primary"><?php echo $username; ?></span>!
   </h1>
-  <p class="text-center p-0 m-0" style="font-size:13pt;">You have used <span
-      class="text-primary-emphasis"><?php echo pouchGetPouches($username, date("m-d-Y")); ?></span> total pouches, and
-    <span class="text-primary-emphasis"><?php echo pouchGetMgs($username, date("m-d-Y")); ?></span> total mgs today.
+  <p class="text-center p-0 m-0" style="font-size:14pt;">You have used <span class="text-primary-emphasis"
+      style="font-size:16pt;"><?php echo pouchGetPouches($username, date("m-d-Y")); ?></span>
+    <?php
+    if (pouchGetPouches($username, date("m-d-Y")) == 1) {
+      echo "total pouch ";
+    } else {
+      echo "total pouches ";
+    } ?>
+    for a total
+    of
+    <span class="text-primary-emphasis"
+      style="font-size:16pt;"><?php echo pouchGetMgs($username, date("m-d-Y")); ?></span> mgs today.
   </p>
 </div>
 <?php
